b1134778b5bff59ce3f1459fa3ad770614293a40
Adriano Pereira Junior <mrninhojr@gmail.com>
2020-05-04 04:47:20 -0300

    Hermes Landing Page - v1.0

---
+++ precache-manifest.4378962f6fe3fe18e5906c206c004842.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e30edc5f731cf73436121d728cbd57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3efdf4d643d62e4e17aa",
+    "url": "/static/css/main.5fc216ec.chunk.css"
+  },
+  {
+    "revision": "d010fcf83865789f90b0",
+    "url": "/static/js/2.66f2310e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.66f2310e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3efdf4d643d62e4e17aa",
+    "url": "/static/js/main.ea0efd7b.chunk.js"
+  },
+  {
+    "revision": "f04e26edfd14a34d5f20",
+    "url": "/static/js/runtime-main.546d5701.js"
+  },
+  {
+    "revision": "e955461a8a89501d1ed6ed81a966c135",
+    "url": "/static/media/chat.e955461a.svg"
+  },
+  {
+    "revision": "e3655abe31c6c3f4ebd0010f41ccf559",
+    "url": "/static/media/data_analysis.e3655abe.svg"
+  },
+  {
+    "revision": "319c4eeccf29596edc0e61cf0017d6a1",
+    "url": "/static/media/deal.319c4eec.svg"
+  },
+  {
+    "revision": "4e78daa331434feb58ae266456efa08b",
+    "url": "/static/media/logo_header.4e78daa3.svg"
+  },
+  {
+    "revision": "6e0bd5a6b998f8036897bd4ad444a4ba",
+    "url": "/static/media/online_shopping.6e0bd5a6.svg"
+  },
+  {
+    "revision": "6fcd71ed0935e89d688a51844be267a2",
+    "url": "/static/media/shopping.6fcd71ed.svg"
+  }
+]);
